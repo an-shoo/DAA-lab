b9936acacc920443926e38640cb36ac63634d141
Abinash <abinash0949@gmail.com>
2024-08-06 17:53:03 +0530

    Update GaleShapeley.java

--- GaleShapeley.java
+++ GaleShapeley.java
@@ -1,81 +1,72 @@
 import java.util.*;
-
-public class GaleShapleyString {
-    static int N = 3; // Number of men and women
-
-    // Function to check if woman w prefers man m1 over her current engagement m
-    boolean wPrefersM1OverM(String[][] prefer, String w, String m, String m1) {
-        int wIndex = indexOf(women, w);
-        for (int i = 0; i < N; i++) {
-            if (prefer[N + wIndex][i].equals(m1))
-                return true;
-            if (prefer[N + wIndex][i].equals(m))
-                return false;
-        }
-        return false;
-    }
-
-    // Helper function to find the index of a string in an array
-    int indexOf(String[] array, String name) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(name)) {
+public class GaleShapeley{
+    static int N=3;
+    int indexof(String[] array,String w){
+        for(int i=0;i<array.length;i++){
+            if(array[i].equals(w)){
                 return i;
             }
         }
-        return -1; // Not found
+        return -1;
     }
-
-    void stableMarriage(String[][] prefer, String[] men, String[] women) {
-        String[] wPartner = new String[N];
-        boolean[] mFree = new boolean[N];
-        Arrays.fill(wPartner, null);
-        int freeCount = N;
-
-        while (freeCount > 0) {
+    boolean wprefersmoverm1(String[][] prefer,String w, String m, String m1){
+        int windex=indexof(women,w);
+        for(int i=0;i<N;i++){
+            if(prefer[N+windex][i].equals(m)){
+                return true;
+            }
+            if(prefer[N+windex][i].equals(m1)){
+            return false;
+        }}
+        return false;
+    }
+    void stablemarriage(String[][] prefer, String[] men, String[] women){
+        String[] wPartner= new String[N];
+        Arrays.fill(wPartner,null);
+        boolean[] mFree= new boolean[N];
+        Arrays.fill(mFree,true);
+        int freecount=N;
+        while(freecount>0){
             int m;
-            for (m = 0; m < N; m++)
-                if (!mFree[m])
-                    break;
-
-            for (int i = 0; i < N && !mFree[m]; i++) {
-                String w = prefer[m][i];
-                int wIndex = indexOf(women, w);
-                int mIndex = indexOf(men, men[m]);
-
-                if (wPartner[wIndex] == null) {
-                    wPartner[wIndex] = men[m];
-                    mFree[m] = true;
-                    freeCount--;
-                } else {
-                    String m1 = wPartner[wIndex];
-                    if (!wPrefersM1OverM(prefer, w, men[m], m1)) {
-                        wPartner[wIndex] = men[m];
-                        mFree[m] = true;
-                        mFree[indexOf(men, m1)] = false;
+            for(m=0;m<N;m++){
+                if(mFree[m])
+                break;
+            }
+            for(int i=0;i<N && mFree[m];i++){
+                String w=prefer[m][i];
+                int windex=indexof(women,w);
+                if(wPartner[windex]==null){
+                    wPartner[windex]=men[m];
+                    mFree[m]=false;
+                    freecount--;
+                }
+                else{
+                    String m1=wPartner[windex];
+                    if(wprefersmoverm1(prefer,w,men[m],m1)){
+                        wPartner[windex]=men[m];
+                        mFree[m]=false;
+                        mFree[indexof(men,m1)]=true;
                     }
                 }
             }
         }
-
-        System.out.println("Woman   Man");
-        for (int i = 0; i < N; i++)
-            System.out.println(women[i] + "  " + wPartner[i]);
+        System.out.println("Woman\tMan");
+        for(int i=0;i<N;i++){
+            System.out.println(women[i]+"\t"+wPartner[i]);
+        }
     }
-
-    static String[] men = {"A", "B", "C"};
-    static String[] women = {"V", "W", "X"};
-
-    public static void main(String[] args) {
+    static String[] men={"A","B","C"};
+    static String[] women={"V","W","X"};
+    public static void main(String[] args){
         String[][] prefer = {
             {"V", "W", "X"}, // A's preferences
-            {"W", "V", "X"}, // B's preferences
-            {"V", "W", "X"}, // C's preferences
+            {"W", "X", "V"}, // B's preferences
+            {"X", "V", "W"}, // C's preferences
             {"A", "B", "C"}, // V's preferences
             {"B", "C", "A"}, // W's preferences
             {"C", "A", "B"}  // X's preferences
         };
-
-        GaleShapleyString gs = new GaleShapleyString();
-        gs.stableMarriage(prefer, men, women);
+        GaleShapeley gs=new GaleShapeley();
+        gs.stablemarriage(prefer,men,women);
     }
 }
