e905532f6cea35bc0fd154399645578633628a9b
Abinash <abinash0949@gmail.com>
2024-08-07 01:19:37 +0530

    Update Counting_Inversions.java

--- Counting_Inversions.java
+++ Counting_Inversions.java
@@ -1,70 +1,63 @@
-
 import java.util.*;
-
-public class tUf {
-
-    private static int merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low;      // starting index of left half of arr
-        int right = mid + 1;   // starting index of right half of arr
-
-        //Modification 1: cnt variable to count the pairs:
-        int cnt = 0;
-
-        //storing elements in the temporary array in a sorted manner//
-
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
+class Solution{
+    public  int mergesort(int[] arr, int low, int high){
+        int invcount=0;
+        if(low>=high) return invcount;
+        int mid=(low+high)/2;
+        invcount+=mergesort(arr,low,mid);
+        invcount+=mergesort(arr,mid+1,high);
+        invcount+=merge(arr,low,mid,high);
+        return invcount;
+    }
+    public static int merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> temp=new ArrayList<>();
+        int invcount=0;
+        int left=low;
+        int right=mid+1;
+        while(left<=mid&&right<=high){
+            if(arr[left]<=arr[right]){
                 temp.add(arr[left]);
                 left++;
-            } else {
+            }
+            else{
                 temp.add(arr[right]);
-                cnt += (mid - left + 1); //Modification 2
+                invcount+=(mid-left+1);
                 right++;
+                
             }
         }
-
-        // if elements on the left half are still left //
-
-        while (left <= mid) {
+        while(left<=mid){
             temp.add(arr[left]);
             left++;
         }
-
-        //  if elements on the right half are still left //
-        while (right <= high) {
+        while(right<=high){
             temp.add(arr[right]);
             right++;
         }
-
-        // transfering all elements from temporary to arr //
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
         }
-        return cnt; // Modification 3
+        return invcount;
     }
-
-    public static int mergeSort(int[] arr, int low, int high) {
-        int cnt = 0;
-        if (low >= high) return cnt;
-        int mid = (low + high) / 2 ;
-        cnt += mergeSort(arr, low, mid);  // left half
-        cnt += mergeSort(arr, mid + 1, high); // right half
-        cnt += merge(arr, low, mid, high);  // merging sorted halves
-        return cnt;
-    }
-
-    public static int numberOfInversions(int[] a, int n) {
-        // Count the number of pairs:
-        return mergeSort(a, 0, n - 1);
-    }
-
-
-    public static void main(String[] args) {
-        int[] a = {5, 4, 3, 2, 1};
-        int n = 5;
-        int cnt = numberOfInversions(a, n);
-        System.out.println("The number of inversions are: " + cnt);
+    public static void main(String[] args){
+        Solution s1=new Solution();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+        }
+        int count=s1.mergesort(arr,0,n-1);
+        System.out.println();
+        for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("No. of inversions: "+count);
     }
 }
-
