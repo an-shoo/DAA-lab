44345c8a96a6a1d96910e6a5665565c1b042a756
Abinash <abinash0949@gmail.com>
2024-08-07 07:32:54 +0530

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 class Solution {
     public static void main(String[] args) {
-        int V = 3, E = 3, S = 2;
+        int V = 5, E = 6, S = 0;
 
         // Creating adjacency list for the graph
         ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
@@ -11,12 +11,12 @@ class Solution {
             adj.add(new ArrayList<>());
         }
         
-        adj.get(0).add(new ArrayList<>(Arrays.asList(1, 1)));
-        adj.get(0).add(new ArrayList<>(Arrays.asList(2, 6)));
-        adj.get(1).add(new ArrayList<>(Arrays.asList(2, 3)));
-        adj.get(1).add(new ArrayList<>(Arrays.asList(0, 1)));
-        adj.get(2).add(new ArrayList<>(Arrays.asList(1, 3)));
-        adj.get(2).add(new ArrayList<>(Arrays.asList(0, 6)));
+        adj.get(0).add(new ArrayList<>(Arrays.asList(1, 10)));
+        adj.get(0).add(new ArrayList<>(Arrays.asList(4, 100)));
+        adj.get(1).add(new ArrayList<>(Arrays.asList(2, 50)));
+        adj.get(2).add(new ArrayList<>(Arrays.asList(3, 20)));
+        adj.get(2).add(new ArrayList<>(Arrays.asList(4, 10)));
+        adj.get(3).add(new ArrayList<>(Arrays.asList(4, 60)));
         
         Solution s1 = new Solution();
         int[] res = s1.dijkstra(V, adj, S);
@@ -25,40 +25,34 @@ class Solution {
             System.out.print(res[i] + " ");
         }
     }
-
-    class Pair {
+    class Pair{
         int node;
-        int distance;
-
-        public Pair(int distance, int node) {
-            this.node = node;
-            this.distance = distance;
+        int dist;
+        public Pair(int dist,int node){
+            this.node=node;
+            this.dist=dist;
         }
     }
-
-    public int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
-        int[] dist = new int[V];
-        Arrays.fill(dist, (int) 1e9);
-        dist[S] = 0;
-        pq.add(new Pair(0, S));
-
-        while (!pq.isEmpty()) {
-            Pair current = pq.poll();
-            int node = current.node;
-            int dis = current.distance;
-
-            for (int i = 0; i < adj.get(node).size(); i++) {
-                int adjNode = adj.get(node).get(i).get(0);
-                int edgeWeight = adj.get(node).get(i).get(1);
-
-                if (dis + edgeWeight < dist[adjNode]) {
-                    dist[adjNode] = dis + edgeWeight;
-                    pq.add(new Pair(dist[adjNode], adjNode));
+    public int[] dijkstra(int V,ArrayList<ArrayList<ArrayList<Integer>>> adj,int S){
+        int[] dis=new int[V];
+        Arrays.fill(dis,999);
+        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((x,y)->x.dist-y.dist);
+        dis[S]=0;
+        pq.add(new Pair(0,S));
+        while(!pq.isEmpty()){
+            Pair current=pq.poll();
+            int node=current.node;
+            int distance=current.dist;
+            for(int i=0;i < adj.get(node).size();i++){
+                int adjnode=adj.get(node).get(i).get(0);
+                int edgeWeight=adj.get(node).get(i).get(1);
+                if(distance+edgeWeight<dis[adjnode]){
+                    dis[adjnode]=distance+edgeWeight;
+                    pq.add(new Pair(dis[adjnode],adjnode));
                 }
             }
         }
-
-        return dist;
+        return dis;
+        
     }
 }
