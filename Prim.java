bf63f2dccfc49cb8dcc60c325d46bb434cba43c1
Abinash <abinash0949@gmail.com>
2024-08-07 07:56:16 +0530

    Update Prim.java

--- Prim.java
+++ Prim.java
@@ -1,20 +1,44 @@
 import java.util.*;
-class Pair{
-    int node;
-    int distance;
-    public Pair(int node,int distance){
-        this.node=node;
-        this.distance=distance;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) {
+        int V = 5, E = 7, S = 0;
+
+        // Creating adjacency list for the graph
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        adj.get(0).add(new ArrayList<>(Arrays.asList(1, 4)));
+        adj.get(0).add(new ArrayList<>(Arrays.asList(3, 8)));
+        adj.get(1).add(new ArrayList<>(Arrays.asList(2, 3)));
+        adj.get(1).add(new ArrayList<>(Arrays.asList(3, 1)));
+        adj.get(2).add(new ArrayList<>(Arrays.asList(3, 7)));
+        adj.get(2).add(new ArrayList<>(Arrays.asList(4, 8)));
+        adj.get(3).add(new ArrayList<>(Arrays.asList(4, 3)));
+        
+        Solution s1 = new Solution();
+        int res = s1.prim(V, adj, S);
+        System.out.println("The Minimum cost achieved is: "+res);
+        
     }
-}
-class Solution{
-    public int prim(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj){
-        PriorityQueue<Pair> pq= new PriorityQueue<Pair> ((x,y)-> x.distance - y.distance);
-        int[] vis= new int[V];
+    class Pair{
+        int node;
+        int distance;
+        public Pair(int node,int distance){
+            this.node=node;
+            this.distance=distance;
+        }
+    }
+    public int prim(int V,ArrayList<ArrayList<ArrayList<Integer>>> adj,int S){
+        int[] vis=new int[V];
         int sum=0;
+        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((x,y)->x.distance-y.distance);
         pq.add(new Pair(0,0));
-        while(pq.size()>0){
-            int node = pq.peek().node;
+        while(!pq.isEmpty()){
+            int node=pq.peek().node;
             int wt=pq.peek().distance;
             pq.remove();
             if(vis[node]==1) continue;
@@ -29,37 +53,5 @@ class Solution{
             }
         }
         return sum;
-    }
-}
-public class code {
-    public static void main(String[] args) {
-        int V = 5;
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-        int[][] edges =  {{0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}};
-
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<ArrayList<Integer>>());
-        }
-
-        for (int i = 0; i < 6; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-
-            ArrayList<Integer> tmp1 = new ArrayList<Integer>();
-            ArrayList<Integer> tmp2 = new ArrayList<Integer>();
-            tmp1.add(v);
-            tmp1.add(w);
-
-            tmp2.add(u);
-            tmp2.add(w);
-
-            adj.get(u).add(tmp1);
-            adj.get(v).add(tmp2);
-        }
-
-        Solution obj = new Solution();
-        int sum = obj.prim(V, adj);
-        System.out.println("The sum of all the edge weights: " + sum);
     }
 }
